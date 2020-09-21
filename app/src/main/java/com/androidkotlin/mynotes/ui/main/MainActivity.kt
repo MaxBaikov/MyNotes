68751b1d4e581a668553d7da3c8a471e84be2a50
Max Baikov <mbaikov@narod.ru>
2020-09-21 17:36:13 +0300

    ДЗ Урок 5 - все готово (есть пара TODO)

--- app/src/main/java/com/androidkotlin/mynotes/ui/main/MainActivity.kt
+++ app/src/main/java/com/androidkotlin/mynotes/ui/main/MainActivity.kt
@@ -4,19 +4,24 @@ package com.androidkotlin.mynotes.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.androidkotlin.mynotes.R
 import com.androidkotlin.mynotes.data.entity.Note
 import com.androidkotlin.mynotes.ui.base.BaseActivity
 import com.androidkotlin.mynotes.ui.note.NoteActivity
+import com.androidkotlin.mynotes.ui.splash.SplashActivity
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
+class MainActivity : BaseActivity<List<Note>?, MainViewState>(), LogoutDialog.LogoutListener {
 
-    companion object{
-        fun start(context: Context) = Intent(context, MainActivity::class.java).apply{
+    companion object {
+        fun start(context: Context) = Intent(context, MainActivity::class.java).apply {
             context.startActivity(this)
         }
     }
@@ -25,23 +30,24 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
-    override  val layoutRes: Int = R.layout.activity_main
+    override val layoutRes: Int = R.layout.activity_main
     lateinit var adapter: NotesRVA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
 
 
 //        setSupportActionBar(toolbar)//Не забудьте поменять в Стилях приложения тему на Theme.AppCompat.Light.NoActionBar - поменял
 
         rv_notes.layoutManager = GridLayoutManager(this, 2)
-        adapter = NotesRVA{
+        adapter = NotesRVA {
             NoteActivity.start(this, it.id)
         }
 
         rv_notes.adapter = adapter
 
-        fab.setOnClickListener{
+        fab.setOnClickListener {
             NoteActivity.start(this)
         }
     }
@@ -51,6 +57,30 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
             adapter.notes = it
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean =
+        MenuInflater(this).inflate(R.menu.main, menu).let { true }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.logout -> showLogoutDialog().let { true }
+            else -> false
+        }
+
+    fun showLogoutDialog() {
+        supportFragmentManager.findFragmentByTag(LogoutDialog.TAG) ?:
+        LogoutDialog().show(supportFragmentManager, LogoutDialog.TAG)
+    }
+
+    override fun onLogout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener{
+                startActivity(Intent(this, SplashActivity::class.java))
+                finish()
+            }
+    }
+
 }
 
 
